157ae0218c878a43d4d244612297f149b3e8a0f6
Finn <finnterdal@gmail.com>
2020-06-22 11:16:00 -0500

    inertial kermit

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+const staticMiddleware = express.static('public');
+app.use(staticMiddleware);
+
+// app.use((req, res, next) => {
+//   console.log(req);
+//   next();
+// });
+
+// app.post() // NEW STUFF
+// app.delete()// DELETE STUFF
+// app.put() // MODIFY STUFF
+// app.get() // RETRIEVE STUFF
+
+app.get('/', (req, res, next) => {
+  res.send(`
+    <head>
+      <link rel="stylesheet" href="/style.css">
+    </head>
+    <h1>Hello there, 2006-FSA-RM!</h1>
+  `);
+});
+
+app.get('/blt', (req, res, next) => {
+  res.json({
+    name: 'BLT',
+    ingredients: [
+      'bacon',
+      'lettuce',
+      'tomato',
+      'bread',
+      'mayo'
+    ]
+  });
+});
+
+let sandwiches = [
+  {
+    id: 1,
+    name: 'BLT',
+  },
+  {
+    id: 2,
+    name: 'Reuben',
+  },
+  {
+    id: 3,
+    name: 'Lox & Cream Cheese',
+  },
+  {
+    id: 4,
+    name: 'Chicken Parm',
+  },
+]
+
+app.get('/sandwiches', (req, res, next) => {
+  res.json(sandwiches);
+})
+
+app.get('/sandwiches/:id', (req, res, next) => {
+  const id = +req.params.id;
+  const sandwich = sandwiches.find((sandwich) => sandwich.id === id);
+  if (!sandwich) {
+    res.sendStatus(404)
+  } else {
+    res.json(sandwich)
+  }
+})
+
+// INSANITY!!!
+// app.get('/sandwiches/1', (req, res, next) => {
+//   res.json(sandwiches[0]);
+// })
+// app.get('/sandwiches/2', (req, res, next) => {
+//   res.json(sandwiches[1]);
+// })
+// app.get('/sandwiches/3', (req, res, next) => {
+//   res.json(sandwiches[2]);
+// })
+
+app.listen(8080, () => {
+  console.log(`Listening on port 8080`);
+});
